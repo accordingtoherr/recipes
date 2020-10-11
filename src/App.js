2c85a8949bed5dd940343c3c23a70ea536c82f0c
accordingtoherr <emilyherr94@gmail.com>
2020-10-10 21:18:29 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+
+
+  const getRecipe = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
+   
+
+    
+    const data = await response.json();
+  
+
+    setRecipe(data.data.children);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+
+
     </div>
   );
 }
